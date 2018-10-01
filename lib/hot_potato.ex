12bd7f8e73f3adadfe6f42a22b776f53bfcc8929
James Norton <jamesnorton@gmail.com>
2018-09-30 20:55:41 -0400

    Remove hot potato as player

--- lib/hot_potato.ex
+++ lib/hot_potato.ex
@@ -46,7 +46,7 @@ defmodule HotPotato do
     IO.puts("Player #{from_player} has requested a game")
     HotPotato.StateManager.start_game(slack, channel, from_player)
     # TODO remove this for real games - needed now for testing by myself
-    HotPotato.StateManager.add_player(slack, channel, slack.me.id)
+    # HotPotato.StateManager.add_player(slack, channel, slack.me.id)
   end
 
   @doc """
